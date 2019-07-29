3436e382f3e294ce0e0ec010f0172bbf031e204f
kjkjv <51977018+kjkjv@users.noreply.github.com>
2019-07-29 18:55:27 +0900

    Add files via upload

--- pandas.py
+++ pandas.py
@@ -4,6 +4,7 @@ import numpy as np
 # 1번
 import pandas as pd
 df = pd.read_csv('boston_train.csv')
+
 df.shape
 df.index
 df.values
@@ -75,17 +76,16 @@ df[df > df['MEDV'].median()]
 MEDV.median()
 
 
-# 3번_??
+# 3번
 df_test = pd.read_csv('boston_test.csv')
 df_test.to_csv('dsfa.csv',index=False)
 
-
 a=df[:11]
 b=df_test[:11]
-
-result=a.append(b)
-print(result)
-result.to_csv('연습이다2222.csv',index=False)     # numpy 파일은 csv로 저장할 수 없음.
+rufrhk = pd.concat([a,b],axis = 0, ignore_index=True)
+print(rufrhk)
+result = df[:11].append(df_test[:11])
+result.to_csv('저장해봤다.csv',index=False)     # numpy 파일은 csv로 저장할 수 없음.
 
 
 # 4번
@@ -111,8 +111,16 @@ print('RM''sum=',RM.sum(),'mean=',RM.mean(),'median=',RM.median(),'min=',RM.min(
 print('AGE''sum=',AGE.sum(),'mean=',AGE.mean(),'median=',AGE.median(),'min=',AGE.min(),'max=',AGE.max())
 print('DIS''sum=',DIS.sum(),'mean=',DIS.mean(),'median=',DIS.median(),'min=',DIS.min(),'max=',DIS.max())
 print('TAX''sum=',TAX.sum(),'mean=',TAX.mean(),'median=',TAX.median(),'min=',TAX.min(),'max=',TAX.max())
-#
 
+
+# 반복은 무조건 for 문을 생각해서 하자 재광아!!!!
+
+for col in df.columns:
+    print(df[col].sum())
+    print(df[col].mean())
+    print(df[col].median())
+    print(df[col].min())
+    print(df[col].max())
 
 
 # ===================================================================================
@@ -127,5 +135,139 @@ print('TAX''sum=',TAX.sum(),'mean=',TAX.mean(),'median=',TAX.median(),'min=',TAX
 # 데이터를 이상적으로 만들어서 그래프를 알맞게 만들어줘야 한다.
 # 분석은 텐선플로 보다 전처리가 중요하다
 # ===================================================================================
+
+
+# 5번
+import quandl
+sunspots = quandl.get('SIDC/SUNSPOTS_A')
+sunspots.columns
+sunspots['Yearly Mean Total Sunspot Number'].isna().sum()
+sunspots['Yearly Mean Standard Deviation'].isna().sum()
+sunspots['Number of Observations'].isna().sum()
+sunspots['Definitive/Provisional Indicator'].isna().sum()
+sunspots.fillna(0)
+sunspots.isna().sum()
+sunspots.to_csv('sunspots.csv')
+# df.fillna() nan 갯수변형
+# df.isnull().values.any()
+
+
+# 6번
+sunspots[Date]
+
+zlzl=pd.read_csv('sunspots.csv')
+zlzl.columns
+zlzl['Date']
+
+result=pd.to_datetime(zlzl['Date'])
+print(result)
+pd.DataFrame.dtypes(result)
+type(result)
+result.mean()
+result[result>result.mean()]
+
+zlzl['Date'] = result[result>result.mean()]
+zlzl.to_csv('sunspot_new2.csv')
+
+
+# 7번
+data = {'판매월':['1월','2월','3월','4월'],
+        '제품A':[100,150,200,130],
+        '제품B': [90,110,140,170]}
+print(data)
+il = pd.DataFrame(data)
+print(il)
+
+data_2 = {'판매월':['1월','2월','3월','4월'],
+        '제품c':[110,130,120,320],
+        '제품d': [130,190,140,130]}
+
+il_2 = pd.DataFrame(data_2)
+
+pd.merge(il,il_2, how='inner', on='판매월')
+
+
+# 8번
+
+gkwk = {'key':['a','b','c'],
+        'left':[1,2,3]}
+gkwk2 = {'key':['a','b','d'],
+        'left':[4,5,6]}
+
+rhkd=pd.DataFrame(gkwk)
+rhkd2=pd.DataFrame(gkwk2)
+
+pd.merge(rhkd,rhkd2,how='inner',on='key')
+pd.merge(rhkd,rhkd2,how='outer')
+pd.merge(rhkd,rhkd2,how='left', on='key')
+pd.merge(rhkd,rhkd2,how='right', on='key')
+
+
+# 9번
+df=pd.read_csv('WHO_first9cols.csv')
+
+print(df[df['Country'].str.contains('Albania')])
+print(df[df['Country'].str.contains('Ethiopia')])
+gg = df['Country'].str.contains('Al')
+
+print(df[gg])                 # 데이터 프레임으로 출력
+print(df['Country'][gg])      # 요소값들로 출력
+
+
+
+# ==================Series 사용 실습 과제===================================
+
+# 문제 1번)
+# Pandas의 Series 데이터구조를 이용하여 다음과 같은 기능을 구현하세요.
+# append()와 contains()메소드 사용
+
+import pandas as pd
+index_col= ['제품','테레비','냉장고','노트북']
+product = pd.Series(['수량',[],[],[]],index=index_col)
+print(product)
+
+print('='*60)
+print('제품수량관리')
+print('1. 입력 2. 출력 3. 검색 9. 종료 중에 메뉴를 선택하세요')
+print('='*60)
+while True:
+    n = int(input('입력하세요'))
+    if n == 1 :
+        while True:
+            p = input('제품명')
+            print(product[p])
+            num = int(input('수량'))
+            product[p].append(num)
+            print(sum(product[p]))
+            a = input('계속 입력? y/n')      # print를 안써도 input은 문구가 뜬다
+            if a == 'y' :                    #''를 안쓰면 변수값이 된다.
+                continue
+            else:
+                break
+    if n == 2 :
+        print(product)
+    elif n == 3 :
+        p = input('제품명')
+        print(product[product.index.str.contains(p)])   # 인덱스값으로 구하는 방법
+    else:
+        n == 9
+        print('종료합니다.')
+        break
+
+# ===============================================================================
+import pandas as pd
+import seaborn as sb
+titanic = sb.load_dataset('titanic')
+
+titanic.columns
+
+titanic.to_csv('titanic_new.csv')
+
+titanic.corr()
+
+
+
+print(product)
+print(input('계속 입력하시겠습니까? Y/N'))
 
 
--- test.py
+++ test.py
@@ -1,32 +1,58 @@
-
-
-print('안녕하세요 배송봇입니다.')
-print('배송을 위해 개인정보를 수집하겠습니다.')
-print('아래의 답변을 적어주세요!')
-
-
-
-while True :
-    agree = input('개인정보 수집에 동의합니까? yes/no')
-    if agree == 'no':
-        print('동의하셔야만 이용이 가능합니다.')
-        break
-    name = input ('성함이 어떻게 되나요?')
-    phone = input('{}님, 연락처가 어떻게 되나요?'.format(name))
-    agree_phone = input('고객님 {} 이 번호가 맞습니까? yes/no'.format(phone))
-
-    elif agree_phone == 'no'
+import pandas as pd
 
 
 
 
 
-    elif agree_phone == 'no':
-        print('다시 입력해주세요.')
-        break
-    else :
-        print('주문이 완료되었습니다. 감사합니다')
-        break
+activity = pd.read_csv('train_activity.csv')
+
+
+
+label = pd.read_csv('train_label.csv')
+
+
+
+print(activity)
+
+print(label)
+
+
+
+print(label.values)
+
+label_1 = label[label['survival_time'] < 8]   # 1일 ~ 7일 그룹
+
+label_2 = label[(label['survival_time'] >= 8)&(label['survival_time'] < 15)]    # 8일 ~ 14일 그룹
+
+label_3 = label[(label['survival_time'] >= 15)&(label['survival_time'] < 22)]
+
+label_4 = label[(label['survival_time'] >= 22)&(label['survival_time'] < 29)]
+
+label_5 = label[(label['survival_time'] >= 29)&(label['survival_time'] < 36)]
+
+label_6 = label[(label['survival_time'] >= 36)&(label['survival_time'] < 43)]
+
+label_7 = label[(label['survival_time'] >= 29)&(label['survival_time'] < 50)]
+
+label_8 = label[(label['survival_time'] >= 50)&(label['survival_time'] < 57)]
+
+label_9 = label[(label['survival_time'] >= 57)&(label['survival_time'] < 64)]
+
+label_10 = label[(label['survival_time'] == 64)]
+
+
+
+# label_1_id = label['acc_id']
+
+# llabel_1_id = label_1_id.tolist()   # acc_id 를 리스트로 만듦
+
+# print(llabel_1_id)
+
+# print(type(llabel_1_id))
+
+
+
+# print(activity.columns)
 
 
 
@@ -34,19 +60,101 @@ while True :
 
 
 
-coffee = 10
-while True:
-    money = int(input("돈을 넣어 주세요: "))
-    if money == 300:
-        print("커피를 줍니다.")
-        coffee = coffee -1
-    elif money > 300:
-        print("거스름돈 %d를 주고 커피를 줍니다." % (money -300))
-        coffee = coffee -1
-    else:
-        print("돈을 다시 돌려주고 커피를 주지 않습니다.")
-        print("남은 커피의 양은 %d개 입니다." % coffee)
-    if coffee == 0:
-        print("커피가 다 떨어졌습니다. 판매를 중지 합니다.")
-        break
+x1 = pd.merge(label_1, activity, how = 'inner', on = 'acc_id')  # 1일 ~ 7일 그룹 사람들을 구별해서 activity와 merge4
+
+x2 = pd.merge(label_2, activity, how = 'inner', on = 'acc_id')
+
+x3 = pd.merge(label_3, activity, how = 'inner', on = 'acc_id')
+
+x4 = pd.merge(label_4, activity, how = 'inner', on = 'acc_id')
+
+x5 = pd.merge(label_5, activity, how = 'inner', on = 'acc_id')
+
+x6 = pd.merge(label_6, activity, how = 'inner', on = 'acc_id')
+
+x7 = pd.merge(label_7, activity, how = 'inner', on = 'acc_id')
+
+x8 = pd.merge(label_8, activity, how = 'inner', on = 'acc_id')
+
+x9 = pd.merge(label_9, activity, how = 'inner', on = 'acc_id')
+
+x10 = pd.merge(label_10, activity, how = 'inner', on = 'acc_id')
+
+
+
+
+
+
+
+a1 = x1.drop(columns = 'amount_spent') # amount_spent 삭제
+
+a2 = x2.drop(columns = 'amount_spent')
+
+a3 = x3.drop(columns = 'amount_spent')
+
+a4 = x4.drop(columns = 'amount_spent')
+
+a5 = x5.drop(columns = 'amount_spent')
+
+a6 = x6.drop(columns = 'amount_spent')
+
+a7 = x7.drop(columns = 'amount_spent')
+
+a8 = x8.drop(columns = 'amount_spent')
+
+a9 = x9.drop(columns = 'amount_spent')
+
+a10 = x10.drop(columns = 'amount_spent')
+
+
+
+
+
+
+
+y1 = a1.groupby('acc_id').mean()[['fishing']]
+
+y2 = a2.groupby('acc_id').mean()[['fishing']]
+
+y3 = a3.groupby('acc_id').mean()[['fishing']]
+
+y4 = a4.groupby('acc_id').mean()[['fishing']]
+
+y5 = a5.groupby('acc_id').mean()[['fishing']]
+
+y6 = a6.groupby('acc_id').mean()[['fishing']]
+
+y7 = a7.groupby('acc_id').mean()[['fishing']]
+
+y8 = a8.groupby('acc_id').mean()[['fishing']]
+
+y9 = a9.groupby('acc_id').mean()[['fishing']]
+
+y10 = a10.groupby('acc_id').mean()[['fishing']]
+
+
+
+xx = y1.sort_values(["acc_id"], ascending=[False])
+
+
+
+z1 = y1.sort_values(["fishing"], ascending=[False])
+
+z2 = y2.sort_values(["fishing"], ascending=[False])
+
+z3 = y3.sort_values(["fishing"], ascending=[False])
+
+z4 = y4.sort_values(["fishing"], ascending=[False])
+
+z5 = y5.sort_values(["fishing"], ascending=[False])
+
+z6 = y6.sort_values(["fishing"], ascending=[False])
+
+z7 = y7.sort_values(["fishing"], ascending=[False])
+
+z8 = y8.sort_values(["fishing"], ascending=[False])
+
+z9 = y9.sort_values(["fishing"], ascending=[False])
+
+z10 = y10.sort_values(["fishing"], ascending=[False])
 
